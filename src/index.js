aa5f8705ec0a61dc31e85b4c319200be3a8fb191
R. Stephens <106496572+RStephens2010@users.noreply.github.com>
2022-07-29 05:36:07 -0600

    Building extended forecast gfx via JS

--- src/index.js
+++ src/index.js
@@ -30,8 +30,11 @@ function formatDate(date) {
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
 
-  forecastElement.innerHTML = `<div class="weather-forecast" id="forecast">
-  <div class="row">
+  let forecastHTML = `<div class="row">`;
+
+  forecastHTML =
+    forecastHTML +
+    `<div class="weather-forecast" id="forecast">
     <div class="col-2">
       <div class="weather-forecast-date">
       Thu</div>
@@ -42,6 +45,24 @@ function displayForecast() {
     </div>
   </div> 
 </div>`;
+
+  forecastHTML =
+    forecastHTML +
+    `<div class="weather-forecast" id="forecast">
+<div class="col-2">
+  <div class="weather-forecast-date">
+  Thu</div>
+  <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" width="42" />
+  <div class="weather-forecast-temperature">
+  <span class="weather-forecast-temperature-max">18°</span> <span class="weather-forecast-temperature-min">12°</span>
+  </div>
+</div>
+</div> 
+</div>`;
+
+  forecastHTML = forecastHTML + `</div>`;
+
+  forecastElement.innerHTML = forecastHTML;
 }
 
 function displayWeatherCondition(response) {
