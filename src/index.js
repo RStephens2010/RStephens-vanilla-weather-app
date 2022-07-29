f28de921154d6a5b58472e7528fdafa037f03d42
R. Stephens <106496572+RStephens2010@users.noreply.github.com>
2022-07-29 02:56:32 -0600

    More JS tweaks to extended forecast - refining innerHTML for 'forecastElement'

--- src/index.js
+++ src/index.js
@@ -30,7 +30,18 @@ function formatDate(date) {
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
 
-  forecastElement.innerHTML = "Forecast";
+  forecastElement.innerHTML = `<div class="weather-forecast" id="forecast">
+  <div class="row">
+    <div class="col-2">
+      <div class="weather-forecast-date">
+      Thu</div>
+      <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" width="42" />
+      <div class="weather-forecast-temperature">
+      <span class="weather-forecast-temperature-max">18°</span> <span class="weather-forecast-temperature-min">12°</span>
+      </div>
+    </div>
+  </div> 
+</div>`;
 }
 
 function displayWeatherCondition(response) {
