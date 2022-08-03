3640a24c03a49fa22c31cb1f9e034dcca8bff124
R. Stephens <106496572+RStephens2010@users.noreply.github.com>
2022-08-03 00:07:57 -0600

    More JS/API tweaking

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ function formatDay(timestamp) {
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "15f77c5dc90e65dac73054881ca1968b";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
 }
 
